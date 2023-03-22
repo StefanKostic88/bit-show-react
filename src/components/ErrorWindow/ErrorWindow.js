9386ff458b13c5b7cc83d3bc230bb210bab05df0
StefanKostic88 <stefankostic88@yahoo.com>
2023-03-22 20:22:20 +0100

    error created

--- src/components/ErrorWindow/ErrorWindow.js
+++ src/components/ErrorWindow/ErrorWindow.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { HiOutlineFaceFrown } from "react-icons/hi2";
 
 const ErrorWindow = ({ errMsg }) => {
   return (
     <div className="pt-4 ">
-      <div>Icon Placeholder</div>
-      <div>{errMsg}</div>
-      Error
+      <div className="container-sm d-flex justify-content-center align-items-center mb-4">
+        <HiOutlineFaceFrown style={{ width: "65px", height: "65px" }} />
+      </div>
+      <div className="container-sm d-flex justify-content-center align-items-center ">
+        <h2 className="text-center w-50">{errMsg}</h2>
+      </div>
     </div>
   );
 };
