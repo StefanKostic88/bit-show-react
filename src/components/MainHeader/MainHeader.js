5f3881c8e22c3d5ae6df7c0047ab2203828e017c
StefanKostic88 <stefankostic88@yahoo.com>
2023-03-23 18:07:09 +0100

    hide search bar on show details page

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,28 +1,27 @@
-import React from "react";
-
+import { useLocation, useParams } from "react-router";
 import { Nav, Navbar, Container, Form } from "react-bootstrap";
 import Canvas from "../Canvas/Canvas";
 
 const MainHeader = () => {
+  const location = useLocation();
+  const { id } = useParams();
+  const showDetailIsOpened = location.pathname === `/all-shows/${id}`;
+
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <Nav>
           <Canvas />
         </Nav>
-        <Form.Group>
-          <Form.Label>Search</Form.Label>
-          <Form.Control placeholder="search" />
-        </Form.Group>
+        {!showDetailIsOpened && (
+          <Form.Group>
+            <Form.Label>Search</Form.Label>
+            <Form.Control placeholder="search" />
+          </Form.Group>
+        )}
       </Container>
     </Navbar>
   );
 };
 
 export default MainHeader;
-
-{
-  /* <Nav.Link href="#home">Home</Nav.Link>
-<Nav.Link href="#features">Features</Nav.Link>
-<Nav.Link href="#pricing">Pricing</Nav.Link> */
-}
